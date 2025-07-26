1d53c3cc720267aeec0d1efcf148551f2da5ffd3
Juan Botero <110292405+JuanBoteroooo@users.noreply.github.com>
2025-07-25 20:28:51 -0400

    playlist

--- src/models/Playlist.js
+++ src/models/Playlist.js
@@ -1,13 +1,9 @@
 import mongoose from 'mongoose';
 
-const playlistSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    idSong: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }],
-    isFavorite: { type: Boolean, default: false } // si es lista de favoritos o no
-  },
-  { timestamps: true }
-);
+const PlaylistSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  idSong: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }]
+}, { timestamps: true });
 
-export default mongoose.model('Playlist', playlistSchema);
+export default mongoose.model('Playlist', PlaylistSchema);
